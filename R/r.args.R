29328abd6062a6b88afc63cf184f6651bb348b3f
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-10-09 18:16:50 +0100

    Full fun

--- R/r.args.R
+++ R/r.args.R
@@ -1,26 +1,3 @@
-bits<-"rnorm(n=1000,sd=29)"
-Cont=TRUE;crit=2;DPQR=TRUE;gen=1;p.val_min=0.05;plot=FALSE;X=rnorm(10)
-
-bits<-"rnorm(n=1000,mean=29)"
-
-bits<-"rnorm(10,11)"
-
-bits<-"rnorm(10)"
-
-bits<-"rnorm(n=10)"
-
-bits<-"rnorm(10,mean=2)"
-
-bits<-"rnorm(10,sd=2)"
-
-bits<-"rnorm(10,sd=2,100)"
-
-bits<-"rnorm(n=10,2,100)"
-
-bits<-"rnorm(n=10,mean=2,100)"
-
-bits<-"FDist(X=rnorm(10), gen = 1, Cont = TRUE, inputNA, plot = FALSE,
-  p.val_min = 0.05, crit = 2, DPQR = TRUE)"
 #' Write the arguments of a function selected
 #'
 #' @return arguments ready to run
@@ -36,7 +13,7 @@ bits<-"FDist(X=rnorm(10), gen = 1, Cont = TRUE, inputNA, plot = FALSE,
 #'
 r.args<-function(){
   ctx <- rstudioapi::getActiveDocumentContext()
-if (!is.null(ctx)) {
+  if (!is.null(ctx)) {
     if (ctx$selection[[1]]$text != "") {
 
       bits <- utils::read.csv(text = ctx$selection[[1]]$text,
@@ -49,7 +26,7 @@ if (!is.null(ctx)) {
         fun<-fun[1:2]
       }
       fun[2]<-gsub(" ","",gsub("\n","",fun[2]))
-      EVALU<-eval(parse(text = (fun[1])))
+      EVALU<-eval(parse(text = fun[1]))
       if(is.function(EVALU)){
         comp_args<-formals(fun)
         args<-names(comp_args)
@@ -63,9 +40,25 @@ if (!is.null(ctx)) {
             fun[2]<-substr(fun[2],1,nchar(fun[2])-1)
           }
           #Separa las comas sii no están entre paréntesis
+          sr<-strsplit(fun[2],"")[[1]]
+          p1<-(1:length(sr))[sr=="("]
+          p2<-(1:length(sr))[sr==")"]
+          c1<-(1:length(sr))[sr==","]
+          ss<-ss2<-c()
+
+          for(i in 1:length(c1)){ss<-c()
+            for(k in 1:(length(p1))){
+              app<-c1[i]>p1[k] & c1[i] <p2[k]
+              ss<-c(ss,app)}
+            ss2<-c(ss2,any(ss))}
+          sr[c1[ss2]]<-"?"
+          fun[2]<-paste0(sr,collapse = "")
 
           values<-strsplit(fun[2],",")[[1]]
+          values<-gsub("[?]",",",values)
+          values<-gsub("==","?",values)
           values2<-strsplit(values,"=")
+          values2<-sapply(values2,function(x){gsub("[?]","==",x)})
 
           len_val<-sapply(values2,length)
           cond<-len_val==2
@@ -93,52 +86,9 @@ if (!is.null(ctx)) {
 
           #print(bits)
           run<-(paste0(run[[2]],collapse=";"))
-          # cond_eq<-(args_no_escritos == sapply(values2,function(x){x[1]}))
-
-
-          #reemplazo
-          # if(cond_eq){
-          #   pred<-pred[!pred[[1]] %in% writ[[1]],]
-          # }
-
-          # pred<-pred[!pred[[1]] %in% writ[[1]],]
-          # #adición
-          # writ<-rbind(writ,pred)
-          #
-          # if(length(args_tofill)>0 && !args_tofill %in% writ[[1]]){
-          #   fill<-data.frame(A=args_tofill,B=paste0(args_tofill,"=",values))
-          #   writ<-rbind(writ,fill)
-          # }
-
-
-          #args_predef_inactivos<-args_predef[!args_predef %in% args_no_escritos]
-          #args_predef_activos les daremos sus valores
-
-          #incl<-comp_args[names(comp_args) %in% args[args %in% args_no_escritos & args %in% args_predef_activos]]
-
-          #si el valor ya está predefinido incluirlo en valores
-          #excepto si ya existe
-          #Le pongo el argumento a los que tienen 1 de long
-          # if(all(cond)){
-          #   run<-paste0(values,collapse = ";")
-          # }else{
-          #   if(!any(cond)){
-          #     run<-paste0(args,"=",values2,collapse = ";")
-          #   }else{#Descubrir qué argumentos faltan
-          #
-          #     # if(!duplicated(c())){
-          #     #   vl<-c(paste0(values,collapse = ";"),paste0(args[!cond],"=",values2[!cond],collapse = ";"))
-          #     #   run<-paste0(vl,collapse=";")
-          #     # }else{
-          #       run<-paste0(c(values[cond],paste0(args_no_escritos,"=",values[!cond])),collapse = ";")
-          #     # }
-          #   }
-          # }
           run<-paste0(run,"\n")
           rstudioapi::modifyRange(ctx$selection[[1]]$range,
                                   run)
-
-
         }
       }
     }
